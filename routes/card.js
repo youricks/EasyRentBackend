654995d7cd589dfc44460db2aedfbf6d2451bc10
You <zjhzyrz@gmail.com>
2017-08-20 21:25:24 +0800

    Revert "Add backend api for charging"
    This reverts commit 829d48f7c5254948ec089169d3d54e371d4395fb.

--- routes/card.js
+++ routes/card.js
@@ -1,16 +1,16 @@
 var express  = require('express');
 var router = express.Router();
 
-var cardDAO = require('../DAO/cardDAO.js');
+var userDao = require('../DAO/cardDAO.js');
 
-router.post('/ticket/purchase',
-	function(req, res, next) {
-    	req.body.amount = req.body.amount * 65
-    	next()
-	}, 
-	cardDAO.purchase, 
-	function(req, res, next) {
-    	res.json({message : req.message, post: req.posts});
-});
+
+
+
+
+
+
+
+
+
 
 module.exports = router;
