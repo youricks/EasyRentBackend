4671f56786aa55805bf7736007f213be68802020
You <zjhzyrz@gmail.com>
2017-08-20 21:29:50 +0800

    Revert "Revert "Add verification api""
    This reverts commit 642daaae13f159e49120b53fd9c0b932c3fc103b.

--- routes/card.js
+++ routes/card.js
@@ -13,4 +13,10 @@ router.post('/ticket/purchase',
     	res.json({message : "Purchase Success"});
 });
 
+router.get('/ticket/verify/:id',
+	cardDAO.verify, 
+	function(req, res, next) {
+    	res.json({message : "Purchase Success"});
+});
+
 module.exports = router;
